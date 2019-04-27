d7c9df21c8cf3bb87686765ce5e33f2c0cd94db4
jaredgav <jaredgav@buffalo.edu>
2019-04-27 15:54:05 -0400

    Created Ability LIbrary / Added Scripts Api folder / First draft movment complete

---
+++ Assets/Scripts/Api/BaseAbility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Api
+{
+    public class BaseAbility : MonoBehaviour
+    {
+        /// <summary>
+        /// Whether or not the ability can be activated
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Whether or not the ability is active
+        /// </summary>
+        public bool ActiveAbility { get; set; }
+
+        public BaseAbility()
+        {
+            Enabled = true;
+            ActiveAbility = false;
+        }
+    }
+}

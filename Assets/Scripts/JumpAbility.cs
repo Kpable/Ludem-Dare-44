4314bed8d0753db65a47de2521c58c9fe82ffe7b
jaredgav <jaredgav@buffalo.edu>
2019-04-27 16:26:06 -0400

    Player Prefab Created / Jump Abilitly In Progress

--- Assets/Scripts/JumpAbility.cs
+++ Assets/Scripts/JumpAbility.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 
 public class JumpAbility : BaseAbility
 {
+    public Rigidbody2D playerBody;
+    public float jumpSpeed;
+
+    /// <summary>
+    /// The jump speed is set to this on Start
+    /// </summary>
+    public float jumpSpeedDefault = 15;
+    private Vector2 CAP_VELOCITY = new Vector2(0, 30);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpSpeed = jumpSpeedDefault;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 pVelocity = playerBody.velocity; 
+            if(pVelocity.y > CAP_VELOCITY.y)
+            {
+                playerBody.velocity += CAP_VELOCITY;
+            }
+            else
+            {
+                playerBody.velocity += Vector2.up * jumpSpeed;
+            }
+        }
     }
 }

eb448856382a5c650bfd54c888e312617c3f3686
lestherreynoso <lhreynos@gmail.com>
2019-04-28 15:51:41 -0400

    player prefab changes

---
+++ Assets/Scripts/OpticsAbility.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Api
+{
+    public class OpticsAbility : BaseAbility
+    {
+        public GameObject optics;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (optics) optics.SetActive(Enabled);
+        }
+
+        public override void SubActivate()
+        {
+            
+        }
+    }
+}

3efcd5894d1c448d5611c90e745983486fac1e20
lestherreynoso <lhreynos@gmail.com>
2019-04-28 15:02:55 -0400

    Platform updates, player updates

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator animator;
+    BasicMovement basicMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        if (animator == null) Debug.LogError("No animator Found");
+        basicMovement = GetComponent<BasicMovement>();
+        if (basicMovement == null) Debug.LogError("No basicMovement Found");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PowerManager.cs
+++ Assets/Scripts/PowerManager.cs
@@ -6,7 +6,7 @@ public class PowerManager : MonoBehaviour
 {
     // Power Params
     public int currentPower = 2;
-    public int maxPower = 2;
+    static public int maxPower = 2;
 
     // Start is called before the first frame update
     void Start()
